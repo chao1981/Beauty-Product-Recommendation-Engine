3eca3e164900203463841c0a9285bf542d2731e5
Ha Luu <luu_baoha@yahoo.com>
2015-06-18 10:04:16 -0400

    update profiler

--- product_user_profile.py
+++ product_user_profile.py
@@ -16,10 +16,13 @@ combination_class = ["combination skin","combination/", "combination oily skin",
 
 sensitive_class = ["sensitive"]
 
-acne_class = ["acne", "break out", "broke out"]
+acne_class = ["acne", "break out", "broke out", "pimple", "black head", "breakout", "blemish", "blackheads"]
 
 anti_aging_class = ["anti-aging", " aging", "wrinkle", "fine lines"]
 
+redness_class = ["redness", "red spot", "rosacea"]
+
+sunscreen_class = ["sunscreen", "sun protection"]
 def category_score(text,category,rating):
     score = 0
     for term in category:
@@ -36,7 +39,9 @@ def product_profiler(item_dict):
     sensitivity = 0
     acne = 0
     aging = 0
-    
+    sunscreen = 0
+    redness = 0
+
     list_of_text = item_dict["Reviews"]
     total_reviews = len(list_of_text)
 
@@ -57,13 +62,17 @@ def product_profiler(item_dict):
         sensitivity += category_score(new_text,sensitive_class,rev_rating)
         acne += category_score(new_text,acne_class,rev_rating)
         aging += category_score(new_text,anti_aging_class,rev_rating)
+        sunscreen += category_score(new_text,sunscreen_class,rev_rating)
+        redness += category_score(new_text,redness_class,rev_rating)
         
     dry_score = dry_skin/smooth_reviews
-    oily_score = oily_skin/smooth_reviews,
+    oily_score = oily_skin/smooth_reviews
     combination_score = combination_skin/smooth_reviews
     sensitivity_score = sensitivity/smooth_reviews
     acne_score = acne/smooth_reviews
     aging_score = aging/smooth_reviews
+    sunscreen_score = sunscreen/smooth_reviews
+    redness_score = redness/smooth_reviews
     
     item_score["dry_skin_score"] = dry_score
     item_score["oily_skin_score"] = oily_score
@@ -71,5 +80,7 @@ def product_profiler(item_dict):
     item_score["sensitive_score"] = sensitivity_score
     item_score["acne_score"] = acne_score
     item_score["anti_aging_score"] = aging_score
+    item_score["sunscreen_score"] = sunscreen_score
+    item_score["redness_score"] = redness
     
     return item_score
